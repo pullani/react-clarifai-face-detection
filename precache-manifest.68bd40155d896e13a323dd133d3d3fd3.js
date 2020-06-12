1f1a4d167467db8f09912d2eac57d0be8142f35c
pullani <mah.pullani@gmail.com>
2020-06-12 21:35:31 +0530

    Updates

---
+++ precache-manifest.68bd40155d896e13a323dd133d3d3fd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194e36c4719dc848942a40808a70bba4",
+    "url": "/react-clarifai-face-detection/index.html"
+  },
+  {
+    "revision": "32d3dafe56700c2d4a34",
+    "url": "/react-clarifai-face-detection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "35e54c4aaf3f2bc44092",
+    "url": "/react-clarifai-face-detection/static/css/main.0a640509.chunk.css"
+  },
+  {
+    "revision": "32d3dafe56700c2d4a34",
+    "url": "/react-clarifai-face-detection/static/js/2.4a5606af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-clarifai-face-detection/static/js/2.4a5606af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e54c4aaf3f2bc44092",
+    "url": "/react-clarifai-face-detection/static/js/main.1f33f8e8.chunk.js"
+  },
+  {
+    "revision": "d6c42aa13f3cf584d140",
+    "url": "/react-clarifai-face-detection/static/js/runtime-main.fb104765.js"
+  }
+]);
