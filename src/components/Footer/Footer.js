f445985ef539ae8e86709d8bf2cf3a9a9ce583d1
pullani <mah.pullani@gmail.com>
2020-06-12 22:44:40 +0530

    footer - _blank

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer style={{position: 'absolute', bottom: '0', width: '100%', height: '2.5rem' }}>
-            <a href='https://github.com/pullani/react-clarifai-face-detection' className='f4 link dim black underline pa3 pointer'>Git-hub</a>
+            <a href='https://github.com/pullani/react-clarifai-face-detection' target="_blank" className='f4 link dim black underline pa3 pointer'>Git-hub</a>
         </footer>
     );
 }
